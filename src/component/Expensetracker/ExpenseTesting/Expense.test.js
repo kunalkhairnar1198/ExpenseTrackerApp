f69a406744ed09d992473024c04b0079c43ef843
kunal khairnar <kunalkhairnar61@gmail.com>
2024-05-08 13:57:20 +0530

    Test 10 test cases in react component Testcases rain and test it correctly 7 test cases outoff 10 test cases

--- src/component/Expensetracker/ExpenseTesting/Expense.test.js
+++ src/component/Expensetracker/ExpenseTesting/Expense.test.js
@@ -4,6 +4,8 @@ import store from "../../../ReduxStore/RtkStore";
 import TotalExpense from "../ExpensItem/TotalExpense";
 import MainNavigation from "../../Layout/MainNavigation";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import userEvent from '@testing-library/user-event'
+import PremiumButton from "../Premium/PremiumButton";
 
 describe('Expense Tracker test component correctly',()=>{
         test('Total Expense test correctly!', () => {
@@ -36,4 +38,77 @@ describe('Expense Tracker test component correctly',()=>{
             expect(homeLinkElement).toBeInTheDocument();
           
           });
+
+          test('clicking SignIn link navigates to the authentication page', () => {
+            render(
+              <Provider store={store}>
+                <BrowserRouter>
+                  <MainNavigation />
+                </BrowserRouter>
+              </Provider>
+            );
+          
+            const signInLink = screen.getByText('SignIn');
+            userEvent.click(signInLink);
+          
+            expect(window.location.pathname).toBe('/authpage');
+          });
+
+          test('renders  if the premium is Activated clicked',()=>{
+            render(
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <PremiumButton />
+                    </BrowserRouter>
+                </Provider>
+            )
+            const premiumButton  = screen.getByText('Premium Activate')
+            userEvent.click(premiumButton)
+            
+        })
+
+        test('renders  if the premium is Activated or not clicked',()=>{
+            render(
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <PremiumButton />
+                    </BrowserRouter>
+                </Provider>
+            )
+            const premiumButton  = screen.getByRole('button')
+            userEvent.click(premiumButton)
+
+            const outputElement = screen.getByText('Download')
+            expect(outputElement).toBeInTheDocument()
+        })
+
+        test('renders mainNavigation',()=>{
+            render(
+                <Provider store={store}>
+                    <BrowserRouter>
+                        <PremiumButton />
+                    </BrowserRouter>
+                </Provider>
+            )
+            const premiumButton  = screen.getByRole('button')
+            userEvent.click(premiumButton)
+
+            const outputElement = screen.getByText('Download')
+            expect(outputElement).toBeInTheDocument()
+        })
+
+        test('clicking Premium Activate button triggers the dark mode handler', () => {
+            render( 
+            <Provider store={store}>
+                <BrowserRouter>
+                    <PremiumButton />
+                </BrowserRouter>
+            </Provider>
+            );
+            
+            const premiumActivateButton = screen.getByRole('button', { name: 'Premium Activate' });
+            userEvent.click(premiumActivateButton);
+          
+        });
+
 })
